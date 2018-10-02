d073967eb94dea0f0efa8fe82bbd7f20e8d2b324
Hadrien Janicot <hadrien.janicot@etu.emse.fr>
2018-10-02 16:34:47 +0200

    Serialisation AJouts

--- SamCar/src/Advert.java
+++ SamCar/src/Advert.java
@@ -8,12 +8,38 @@ Advert for SamCar
  */
 
 //Imports list
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.*;
 
-public class Advert {
+public class Advert implements Serializable {
     private UserProfil adCreator;
     private Event adEvent;
     private boolean sam;
     private int carSize;
     private List<UserProfil> adMembers;
+
+    @Override
+    public byte[] serialize() throws IOException {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        ObjectOutputStream os = new ObjectOutputStream(out);
+        os.writeObject(this);
+        return out.toByteArray();
+    }
+
+    @Override
+    public void deserialize(byte[] data) throws IOException, ClassNotFoundException {
+        ByteArrayInputStream in = new ByteArrayInputStream(data);
+        ObjectInputStream is = new ObjectInputStream(in);
+        Advert tampon = new Advert();
+        tampon = (Advert) is.readObject();
+        this.adCreator = tampon.adCreator;
+        this.adEvent = tampon.adEvent;
+        this.adMembers = tampon.adMembers;
+        this.carSize = tampon.carSize;
+        this.sam = tampon.sam;
+    }
 }
--- SamCar/src/Event.java
+++ SamCar/src/Event.java
@@ -8,11 +8,36 @@ Event on which ad can be done
  */
 
 //Imports list
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import javax.swing.*;
 
-public class Event {
+public class Event implements Serializable{
     private String eventName;
     private String adress;
     private String date;
     private ImageIcon eventPicture;    
+
+    @Override
+    public byte[] serialize() throws IOException {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        ObjectOutputStream os = new ObjectOutputStream(out);
+        os.writeObject(this);
+        return out.toByteArray();
+    }
+
+    @Override
+    public void deserialize(byte[] data) throws IOException, ClassNotFoundException {
+            ByteArrayInputStream in = new ByteArrayInputStream(data);
+        ObjectInputStream is = new ObjectInputStream(in);
+        Event tampon = new Event();
+        tampon = (Event) is.readObject();
+        this.adress = tampon.adress;
+        this.date = tampon.date;
+        this.eventName = tampon.eventName;
+        this.eventPicture = tampon.eventPicture; 
+    }
 }
