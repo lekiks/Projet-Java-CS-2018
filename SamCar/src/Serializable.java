253c4e2552e47e79887eedd5d637a2d62975841c
Hadrien Janicot <hadrien.janicot@etu.emse.fr>
2018-10-02 16:34:31 +0200

    Serialisation AJouts

---
+++ SamCar/src/Serializable.java
@@ -0,0 +1,17 @@
+
+import java.io.IOException;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hadrienjanicot
+ */
+public interface Serializable {
+     byte[] serialize() throws IOException;
+     void deserialize(byte[] data) throws IOException, ClassNotFoundException;  
+}
